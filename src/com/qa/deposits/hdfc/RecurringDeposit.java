21f5d0b2d27ea80f85e280814f1668cf010eeffb
Surya8792 <88296765+Surya8792@users.noreply.github.com>
2023-03-01 09:43:55 +0530

    Update RecurringDeposit.java

--- src/com/qa/deposits/hdfc/RecurringDeposit.java
+++ src/com/qa/deposits/hdfc/RecurringDeposit.java
@@ -3,4 +3,13 @@ package com.qa.deposits.hdfc;
 public class RecurringDeposit 
 {
   System.out.println("RD");
+  System.out.println("RD");
+  System.out.println("RD");
+  System.out.println("RD");
+  
 }
+public static void main(String [] args)
+{
+  RecurringDeposit obj = new RecurringDeposit();
+}
+
